1026a5035b03674288e559270b38c3aa0e137ae3
Divue <divue2003@gmail.com>
2024-06-05 23:45:58 +0530

    Day 1 of Learning React

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>Chai in React | Divue</h2>
+    )
+}
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>   //Fragment
+      <Chai/>
+      <h1>Two tags can return in an empty tag called fragment</h1>
+    </>
+
+)
+}
+
+export default App
