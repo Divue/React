2732e77ad328d2f2dd57c5a7e5a61fb336037cc0
Divue <divue2003@gmail.com>
2024-06-06 13:58:09 +0530

    lec 4   Create your own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,15 @@
 import Chai from "./chai"
 function App() {
+  const username = "chai aur code"
 
   return (
     <>   //Fragment
       <Chai/>
-      <h1>Two tags can return in an empty tag called fragment</h1>
-    </>
+      <h1>Two tags can return in an empty 
+        tag called fragment. {username}  </h1>
+          // {} this is EVALUATED EXPRESSION means only the final output will work not if else etc..
+          all the if else other things should operater before return and inside function App()
+    </> 
 
 )
 }
